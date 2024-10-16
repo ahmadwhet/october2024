ba31f9f264918823f16fb4c29d47963a4941c2e6
ahmadwhet <ahmadws18@gmail.com>
2024-10-16 21:59:50 +0700

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = "./data.json";
 
 const makeCommits = (n) => {
     if(n===0) return simpleGit().push();
-    const x = random.int(0,54);
+    const x = random.int(0,-54);
     const y = random.int(0,6);
     const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
 
@@ -21,4 +21,4 @@ jsonfile.writeFile(path, data, () => {
 });
 };
 
-makeCommits(100);
+makeCommits(300);
