8847304e2f493b717d01afff7f1b3bec4a79227e
ahmadwhet <ahmadws18@gmail.com>
2024-10-16 20:59:52 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   data.json
    	modified:   index.js
    	modified:   node_modules/.package-lock.json

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
 import random from "random";
-
 const path = "./data.json";
-const date = moment().format();
 
-const markCommit = (x, y) => {
-    const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+const makeCommits = (n) => {
+    if(n===0) return simpleGit().push();
+    const x = random.int(0,54);
+    const y = random.int(0,6);
+    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+
 
 const data = {
     date: date,
@@ -20,6 +17,8 @@ const data = {
 
 console.log(date);
 jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date,{'--date':date}).push();
+    simpleGit().add([path]).commit(date,{'--date':date},makeCommits.bind(this,--n));
 });
 };
+
+makeCommits(100);
